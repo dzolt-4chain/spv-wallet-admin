abf2b60340d464593370a07a6f8bad8d060ad4fe
swierzbicki <sebastian.wierzbicki@circlekeurope.com>
2024-10-07 10:36:45 +0200

    todo

--- src/utils/xPubQueryOptions.tsx
+++ src/utils/xPubQueryOptions.tsx
@@ -16,7 +16,28 @@ export const xPubQueryOptions = (opts: XPubQueryOptions) => {
     queryFn: async () => {
       console.log('--- QUERING: --- ');
 
-      return fetch('/admin/xpubs/search').then((res) => res.json());
+      return fetch('/admin/xpubs/search').then((res) => {
+        return [
+          {
+            created_at: new Date('2024-08-02T09:22:06.184649Z'),
+            updated_at: new Date('2024-08-02T09:22:06.184649Z'),
+            id: 'e7d96399df6400de6a16d34adf63fcb500b0b6cd9f016db4b8201b99d24679df',
+            current_balance: 0,
+            next_internal_num: 0,
+            next_external_num: 1,
+            status: '',
+          },
+          {
+            created_at: new Date('2024-08-02T09:22:06.184649Z'),
+            updated_at: new Date('2024-08-02T09:22:06.184649Z'),
+            id: '378aa9182fc61b1c6b8e132bb6f1b52f3c85320407d259a95a8cf2dcbbfa9d8c',
+            current_balance: 121,
+            next_internal_num: 8,
+            next_external_num: 2,
+            status: '',
+          },
+        ];
+      });
     },
   });
 };
